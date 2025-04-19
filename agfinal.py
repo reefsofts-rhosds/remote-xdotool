b68ad2ad23a23ff752c74fad6a7df8426b432b86
reefsofts-rhosds <youri.lkh@girofle.org>
2025-04-19 14:15:23 +0200

    Added a final.py for easier use of ALL keypresses

--- agfinal.py
+++ agfinal.py
@@ -14,36 +14,7 @@ class Key:
     def __init__(self, name):
         self.name = name
 
-keys = [
-    Key("0"), Key("1"), Key("2"), Key("3"), Key("4"),
-    Key("5"), Key("6"), Key("7"), Key("8"), Key("9"),
-    Key("A"), Key("B"), Key("C"), Key("D"), Key("E"),
-    Key("F"), Key("G"), Key("H"), Key("I"), Key("J"),
-    Key("K"), Key("L"), Key("M"), Key("N"), Key("O"),
-    Key("P"), Key("Q"), Key("R"), Key("S"), Key("T"),
-    Key("U"), Key("V"), Key("W"), Key("X"), Key("Y"),
-    Key("Z"),
-    Key("F1"), Key("F2"), Key("F3"), Key("F4"), Key("F5"),
-    Key("F6"), Key("F7"), Key("F8"), Key("F9"), Key("F10"),
-    Key("F11"), Key("F12"),
-    Key("Shift"), Key("Control"), Key("Ctrl"), Key("Alt"),
-    Key("AltGr"), Key("Meta"), Key("Windows"), Key("Caps Lock"),
-    Key("Num Lock"), Key("Scroll Lock"),
-    Key("Arrow Up"), Key("Arrow Down"), Key("Arrow Left"),
-    Key("Arrow Right"), Key("Home"), Key("End"),
-    Key("Page Up"), Key("Page Down"), Key("Insert"),
-    Key("Delete"), Key("Space"), Key("Enter"), Key("Tab"),
-    Key("Backspace"), Key("Escape"), Key("Esc"),
-    Key("Print Screen"), Key("Pause"), Key("Break"),
-    Key("Numpad 0"), Key("Numpad 1"), Key("Numpad 2"),
-    Key("Numpad 3"), Key("Numpad 4"), Key("Numpad 5"),
-    Key("Numpad 6"), Key("Numpad 7"), Key("Numpad 8"),
-    Key("Numpad 9"), Key("Numpad +"), Key("Numpad -"),
-    Key("Numpad *"), Key("Numpad /"), Key("Numpad ."),
-    Key("Application"), Key("Volume Up"), Key("Volume Down"),
-    Key("Mute"), Key("Media Play/Pause"), Key("Media Stop"),
-    Key("Media Next"), Key("Media Previous")
-]
+keys = [Key("Up"), Key("Down")]
 
 
 for key in keys:
